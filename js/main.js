5f9b6d045f1f1d0c0efd889ca49cd6bf52d27199
MilanSarker <137993982+MilanSarker@users.noreply.github.com>
2023-09-22 04:14:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".banner_carousel").owlCarousel({
+        items:1,
+        nav:true,
+        loop:true,
+        navText:['<span><i class="fa-solid fa-arrow-left"></i></span>','<span><i class="fa-solid fa-arrow-right"></i></span>']
+    });
+  });
+
+
+$(document).ready(function(){
+    $(".brand_carousel").owlCarousel({
+        items:5,
+        nav:true,
+        loop:true,
+       navText:['<span><i class="fa-solid fa-arrow-left"></i></span>','<span><i class="fa-solid fa-arrow-right"></i></span>']
+    });
+  });
